d596c2ebf27130431eb3f5c8811b83c0804f2bc1
Akki-net <akashsharma.zak@gmail.com>
2024-06-15 16:22:27 +0530

    image-center

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,15 +5,12 @@ import image from '../assets/img-1.png'
 const Content = () => {
   return (
     <div className="content-container">
-       <div className="content-title-container">
+      <div className="content-title-container">
         <h3 className='content-head'>Award Winning</h3>
         <h1 className='content-title'>Leading Law Firm</h1>
         <h5 className='content-text'> We debounce with righteous indigination and dislike men who are beguiled and demoralized by the charms of pleas</h5>
       </div>
-      <div className="box">
-        <img src={image} alt="" />
-      </div>
-     
+      <img style={{ maxHeight: "34rem" }} src={image} alt="" />
     </div>
 
 
